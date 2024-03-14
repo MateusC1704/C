de1cf9a5394012775f928cb6ec64b1aa5b5ba3c0
MateusC1704 <163435614+MateusC1704@users.noreply.github.com>
2024-03-14 12:22:57 -0300

    PPTLG.c

--- PPTLG.c
+++ PPTLG.c
@@ -1,3 +1,7 @@
+/*Jogo "Pedra Papel Tesoura Lagarto Spock" contra a mÃ¡quina. 
+Feito em linguagem C por Mateus C. Cardoso.
+Baseado no jogo original "Rock Paper Scissors Spock Lizard", de Sam Kass e Karen Bryla.
+*/
 #include<stdio.h>
 #include<locale.h>
 int main(){
@@ -6,21 +10,21 @@ int main(){
 	
 	do {
 	system("cls");
-	printf("- - - - - - - - Pedra, Papel, Tesoura, Lagarto, Spock - - - - - - -\n- - - - - Jogo original criado por Sam Kass e Karen Bryla - - - - -\n");
-	printf("\n1 - Pedra\n2 - Papel\n3 - Tesoura\n4 - Lagarto\n5 - Spock\n\n0 - Ajuda\n\nFaça sua escolha (1-5): ");
+	printf("- - - - - - - - Pedra Papel Tesoura Lagarto Spock - - - - - - -\n- - - - - Jogo original criado por Sam Kass e Karen Bryla - - - - -\n");
+	printf("\n1 - Pedra\n2 - Papel\n3 - Tesoura\n4 - Lagarto\n5 - Spock\n\n0 - Ajuda\n\nFaÃ§a sua escolha (1-5): ");
 	scanf("%d",&jogador);
 
 	while(jogador==0)	{
 		printf("\n-Tesoura corta papel\n-Papel cobre pedra\n-Pedra esmaga lagarto\n-Lagarto envenena Spock\n-Spock esmaga tesoura\n-Tesoura decapita Lagarto\n-Lagarto come papel\n-Papel refuta Spock\n-Spock vaporiza pedra\n-Pedra amassa tesoura");
-		printf("\nFaça sua escolha (1-5): ");
+		printf("\nFaÃ§a sua escolha (1-5): ");
 		scanf("%d",&jogador);
 	}
 
-	if(jogador==1) printf("\nVocê escolheu Pedra");
-	else if(jogador==2) printf("\nVocê escolheu Papel");
-	else if(jogador==3) printf("\nVocê escolheu Tesoura");
-	else if(jogador==4) printf("\nVocê escolheu Lagarto");
-	else if(jogador==5) printf("\nVocê escolheu Spock");
+	if(jogador==1) printf("\nVocÃª escolheu Pedra");
+	else if(jogador==2) printf("\nVocÃª escolheu Papel");
+	else if(jogador==3) printf("\nVocÃª escolheu Tesoura");
+	else if(jogador==4) printf("\nVocÃª escolheu Lagarto");
+	else if(jogador==5) printf("\nVocÃª escolheu Spock");
 	
 	srand(time(NULL));
 	bot=(rand() % 5)+1;
@@ -33,11 +37,11 @@ int main(){
 	
 	if(jogador==bot) printf("\nEmpate!\n");
 	else if((jogador==3 && bot==2)||(jogador==2 && bot==1)||(jogador==1 && bot==4)||(jogador==4 && bot==5)||(jogador==5 && bot==3)||(jogador==3 && bot==4)||(jogador==4 && bot==2)||(jogador==2 && bot==5)||(jogador==5 && bot==1)||(jogador==1 && bot==3)) {
-		printf("\nVocê venceu!!!\n");
+		printf("\nVocÃª venceu!!!\n");
 	}
 	else printf("\nO bot venceu!\n");
 	
-	printf("\nDeseja continuar? \n1 - SIM | 2 - NÃO\n ");
+	printf("\nDeseja continuar? \n1 - SIM | 2 - NÃƒO\n ");
 	scanf("%d",&e);
 	} while (e==1);
 	

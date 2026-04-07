67287e27df11132de9471b184b3333199e0421bb
Mateus C. Cardoso <163435614+MateusC1704@users.noreply.github.com>
2026-04-07 14:49:48 -0300

    Calculador de possibilidades
    Calculador de possibilidades básico, versão 1.0

---
+++ possibilometro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int p;
+	int *vetor;
+	char numeros[];
+/*	printf("Digite o numero de possiblidades: ");
+	scanf(%d,&p);*/
+	printf("Digite os numeros:");
+	scanf(%c,&numeros);
+	
+	printf(strlen(numero));
+	
+	return 0;
+}
